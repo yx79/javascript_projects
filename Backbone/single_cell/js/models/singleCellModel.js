fecf6709310bcad7f16b13caa14a711ee54a5e71
yx79 <yx79@georgetown.edu>
2017-01-14 19:02:47 -0500

    Add functions

--- Backbone/single_cell/js/models/singleCellModel.js
+++ Backbone/single_cell/js/models/singleCellModel.js
@@ -4,6 +4,10 @@ app.singleCell = Backbone.Model.extend({
 	defaults: {
 		color: "blue",
 		img: "images/placeholder.jpg"
+	},
+
+	initialize: function() {
+		console.log("A model isntance named " + this.get("name") + " has been created.");
 	}
 
 })
